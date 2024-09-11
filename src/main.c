2544398d3f2d8d478a72c3a6b1a00cfcd40f4529
hozlucas28 <hozlucas28@gmail.com>
2024-09-11 00:30:45 -0300

    test(ci): format code action

--- src/main.c
+++ src/main.c
@@ -26,6 +26,13 @@ int main() {
 
     fillDashboard(&game, 0);
 
+
+
+
+
+
+
+
     requestedPattern = getUserInputStr("Which pattern do you want (XXX)? ", 100,
                                        &validatePattern);
     printf("\n'%s'", requestedPattern);
