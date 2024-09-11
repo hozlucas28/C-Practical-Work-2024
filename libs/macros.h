9f030b9543fc1447c2e663c8b0f7bac840f635cc
hozlucas28 <hozlucas28@gmail.com>
2024-09-11 00:14:02 -0300

    feature(macros): add ROWS and COLS

--- libs/macros.h
+++ libs/macros.h
@@ -1,4 +1,28 @@
 #ifndef MACROS_H_INCLUDED
 #define MACROS_H_INCLUDED
 
+/**
+ * @def ROWS
+ * @brief Defines the number of rows in a grid.
+ *
+ * This macro is used to define the number of rows in a grid.
+ * It is typically used in conjunction with the COLS macro to define the size of
+ * a grid.
+ *
+ * @warning The value of ROWS should be a positive integer.
+ */
+#define ROWS 56
+
+/**
+ * @def COLS
+ * @brief Defines the number of columns in a grid.
+ *
+ * This macro is used to define the number of columns in a grid.
+ * It is typically used in conjunction with the ROWS macro to define the size of
+ * a grid.
+ *
+ * @warning The value of COLS should be a positive integer.
+ */
+#define COLS 110
+
 #endif  // MACROS_H_INCLUDED
