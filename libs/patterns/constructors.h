d93965548481757fec08ffb9f46e2ac5f5acf0ec
hozlucas28 <hozlucas28@gmail.com>
2024-09-12 20:48:49 -0300

    doc(newGliderPattern): add missing documentation

--- libs/patterns/constructors.h
+++ libs/patterns/constructors.h
@@ -4,7 +4,15 @@
 
 #include "./structs.h"
 
-// TODO: Documentation
+/**
+ * @brief Initializes a new glider pattern.
+ *
+ * This function sets up a new glider pattern in the provided TPattern structure.
+ * The glider pattern is a small configuration of cells that travels across a Conway's
+ * Game of Life grid.
+ *
+ * @param pattern Pointer to the TPattern structure to be initialized with the glider pattern.
+ */
 void newGliderPattern(TPattern* pattern);
 
 // TODO: Documentation
