8ec4da71e72f2dfeec7279cb7fc4205e9e982ae1
hozlucas28 <hozlucas28@gmail.com>
2024-09-12 20:30:46 -0300

    doc(fillPattern): add missing documentation

--- libs/patterns/methods.h
+++ libs/patterns/methods.h
@@ -4,7 +4,14 @@
 
 #include "./structs.h"
 
-// TODO: Documentation
+/**
+ * @brief Fills the `arr` field of a pattern structure with a specified value.
+ *
+ * @param pPattern A pointer to the pattern structure.
+ * @param with The value to fill the `arr` field.
+ *
+ * @warning This function assumes that `pPattern` has been properly initialized.
+ */
 void fillPattern(TPattern* pPattern, char with);
 
 // TODO: Documentation
