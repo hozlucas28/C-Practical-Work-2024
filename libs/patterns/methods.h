ee3da8a0d355d427acfa12f0023cfb51a39cacb6
hozlucas28 <hozlucas28@gmail.com>
2024-09-12 20:32:50 -0300

    doc(printPatternByConsole): add missing documentation

--- libs/patterns/methods.h
+++ libs/patterns/methods.h
@@ -14,7 +14,13 @@
  */
 void fillPattern(TPattern* pPattern, char with);
 
-// TODO: Documentation
+/**
+ * @brief Prints the `arr` field of a pattern structure by console.
+ *
+ * @param pPattern A pointer to the pattern structure.
+ *
+ * @warning This function assumes that `pPattern` has been properly initialized.
+ */
 void printPatternByConsole(TPattern* pPattern);
 
 #endif  // PATTERNS_METHODS_H_INCLUDED
