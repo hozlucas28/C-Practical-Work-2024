73d26f14f10849a66cd60592898cb28a873c58b4
hozlucas28 <hozlucas28@gmail.com>
2024-09-15 03:43:33 -0300

    feature: add `ALIVE_CELL_NG` and `DEAD_CELL_NG` macros
    Co-authored-by: Guidolinares <guidolina@users.noreply.github.com>
    Co-authored-by: tefhuergo <tefhuergo@users.noreply.github.com>
    Co-authored-by: Tiago Giannotti <tiago.giannotti.02@gmail.com>

--- libs/patterns/macros.h
+++ libs/patterns/macros.h
@@ -11,12 +11,30 @@
 #define ALIVE_CELL 'o'
 
 /**
+ * @def ALIVE_CELL_NG
+ * @brief Represents a cell that is going to be alive in the next generation.
+ *
+ * @warning The value of `ALIVE_CELL_NG` must be a single character and must not be equal to
+ * `ALIVE_CELL`, `DEAD_CELL`, or `DEAD_CELL_NG` macros.
+ */
+#define ALIVE_CELL_NG '1'
+
+/**
  * @def DEAD_CELL
  * @brief Represents a dead cell.
  *
  * @warning The value of `DEAD_CELL` must be a single character.
  */
 #define DEAD_CELL ' '
+
+/**
+ * @def DEAD_CELL_NG
+ * @brief Represents a cell that is going to be dead in the next generation.
+ *
+ * @warning The value of `DEAD_CELL_NG` must be a single character and must not be equal to
+ * `ALIVE_CELL`, `ALIVE_CELL_NG`, or `DEAD_CELL` macros.
+ */
+#define DEAD_CELL_NG '0'
 
 /**
  * @def PATTERN_ROWS
